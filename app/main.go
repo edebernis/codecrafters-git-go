b85895b3bf2faa27070a2f7d559940b7bc409e2f
Emeric de Bernis <emeric.debernis@qonto.com>
2022-05-16 10:19:35 +0200

    stage 1

--- app/main.go
+++ app/main.go
@@ -10,14 +10,14 @@ import (
 func main() {
 	switch command := os.Args[1]; command {
 	case "init":
-		init()
+		handleInit()
 	default:
 		fmt.Println("Unknown command %s", command)
 		os.Exit(1)
 	}
 }
 
-func init() {
+func handleInit() {
 	for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
 		if err := os.Mkdir(dir, 0755); err != nil {
 			fmt.Printf("Error creating directory: %s\n", err)
